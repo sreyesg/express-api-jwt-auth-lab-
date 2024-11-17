3664c2cf09861d386ea6f217fae7376ca6f4462d
sreyesg <santiagoreyes.g@gmail.com>
2024-11-17 10:11:52 -0800

    Refactor signup and signin routes

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 const SALT_LENGTH = 10
 
@@ -16,6 +17,9 @@ router.post('/signup', async(req, res) => {
             username: req.body.username,
             hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH)
         })
+        const token = jwt.sign(
+            {username: user.username, _id: user._id},
+            process.env.JWT_SECRET)
         res.json(user)
         
     } catch (error) {
@@ -23,5 +27,19 @@ router.post('/signup', async(req, res) => {
     }
 })
 
+router.post('/signin', async(req, res) => {
+    try {
+        
+        const user = await User.findOne({username: req.body.username})
+         if(user && bcrypt.compareSync(req.body.password, user.hashedPassword)){
+            const token = jwt.sign(
+                {username: user.username, _id: user._id},
+                process.env.JWT_SECRET)
+         }
+    } catch (error) {
+        console.log(error)
+    } 
+
+})
 
 module.exports = router
