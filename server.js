7c6b0715751a1ffde189966de92896a000e1f717
sreyesg <santiagoreyes.g@gmail.com>
2024-11-17 10:36:21 -0800

    Update upstream

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const mongoose = require('mongoose');
 const testJWTRouter = require('./controllers/test-jwt')
 const usersRouter = require('./controllers/users')
+const profilesRouter = require('./controllers/profiles')
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -15,6 +16,8 @@ mongoose.connection.on('connected', () => {
 app.use(express.json());
 app.use('/test-jwt', testJWTRouter)
 app.use('/users', usersRouter)
+app.use('/profiles', profilesRouter)
+
 // Routes go here
 
 app.listen(3000, () => {
