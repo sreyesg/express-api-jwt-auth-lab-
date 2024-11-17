e5dbf9b123c7b04f4a21d817e0235d75f44efa8f
sreyesg <santiagoreyes.g@gmail.com>
2024-11-16 18:29:54 -0800

    Refactor signup route

--- controllers/users.js
+++ controllers/users.js
@@ -6,15 +6,21 @@ const User = require('../models/user')
 const SALT_LENGTH = 10
 
 router.post('/signup', async(req, res) => {
-    const userInDatabase = await User.findOne({ username: req.body.username})
-    if(userInDatabase){
-        return res.json(400),json({mesage: "Username already exits"})
+    
+    try {
+        const userInDatabase = await User.findOne({ username: req.body.username})
+        if(userInDatabase){
+            return res.json(400),json({mesage: "Username already exits"})
+        }
+        const user = await User.create({
+            username: req.body.username,
+            hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH)
+        })
+        res.json(user)
+        
+    } catch (error) {
+        console.log(error)
     }
-    const user = await User.create({
-        username: req.body.username,
-        hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH)
-    })
-    res.json({message: "caca"})
 })
 
 
